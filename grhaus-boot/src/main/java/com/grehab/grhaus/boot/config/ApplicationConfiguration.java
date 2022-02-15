23d876ac9aaf8381bbcc000b7628fc8dd1d1e7d4
ufranco <ulisesjavierfranco@gmail.com>
2022-02-15 00:32:00 -0300

    [GRHAUS-1] implemented swagger-ui

--- grhaus-boot/src/main/java/com/grehab/grhaus/boot/config/ApplicationConfiguration.java
+++ grhaus-boot/src/main/java/com/grehab/grhaus/boot/config/ApplicationConfiguration.java
@@ -1,10 +1,8 @@
 package com.grehab.grhaus.boot.config;
 
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class ApplicationConfiguration {
 
 }
